451d7740ba1f514df22f76cfbb84735cb2150916
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2025-08-23 07:33:15 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -53,7 +53,7 @@ app.get('/api/seed', async (req, res) => {
     // 3. (Opcional) Crear un registro de progreso para ese usuario
     const testProgress = new Progress({
       user: testUser._id,
-      taskName: 'Lección de Prueba',
+      lessonName: 'Lección de Prueba',
       score: 100,
       completed: true
     });
@@ -184,4 +184,5 @@ app.get('/api/progress/:userId', async (req, res) => {
 
 // --- 7. Export de la App ---
 module.exports = app;
+
 
